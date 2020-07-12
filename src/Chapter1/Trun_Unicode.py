f2ddf7e321d7b2b70ebad8d7ed926608b8e521f2
SinglePledge <974165255@qq.com>
2020-07-13 01:42:24 +0800

    compare speed of normal or filter with map

--- src/Chapter1/Trun_Unicode.py
+++ src/Chapter1/Trun_Unicode.py
@@ -14,3 +14,17 @@ beyond_ascii = [ord(s) for s in symbols if ord(s) > 33]
 print(beyond_ascii)
 beyond_ascii = list(filter(lambda c:c>33,map(ord ,symbols)))
 print(beyond_ascii)
+# if you need a list , in this list, it has 3 different type of T-shirt,each size have 2 type of color
+colors = ['blue','red']
+sizes = ['S','M','L']
+#order by color
+tshirts = [(color,size) for color in colors for size in sizes]
+print(tshirts)
+#It's same order as last one
+for color in colors:
+    for size in sizes:
+        print((color,size))
+#Order by size
+tshirts = [(color,size) for size in sizes
+                        for color in colors]
+print(tshirts)
