5b7b3769a2dfba3c78ec275e371db60ddd27fcde
SinglePledge <974165255@qq.com>
2020-07-16 02:35:24 +0800

    chapter2

--- src/Chapter2/Slice.py
+++ src/Chapter2/Slice.py
@@ -6,3 +6,10 @@ print(l[2:])
 print(l[:3])#在下标3的地方分割
 print(l[3:])
 #list[start:stop],返回下标为stop-start
+
+#对象切片,s[a:b:c],对s在a和b之间以c为间隔取值
+s='bicycle'
+print(s[::3])
+print(s[::-1])
+print(s[::-2])
+#seq[start:stop:step]求值,python会调用seq.__getitem__(slice(
