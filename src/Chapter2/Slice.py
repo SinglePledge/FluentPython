b8bb6097ec6752f9e2495bde1219d5c8532971a6
SinglePledge <974165255@qq.com>
2020-07-20 03:22:11 +0800

    init commit

--- src/Chapter2/Slice.py
+++ src/Chapter2/Slice.py
@@ -33,3 +33,47 @@ for item in line_items:
 #[]运算符里还可以使用逗号分开多个索引或者是切片
 
 #如果将切片放在赋值语句的左边,或把它作为del操作的对象,我们就可以对序列进行嫁接,切除或就地修改操作
+l1 = list(range(10))
+print(l1)
+l1[2:5] = [20, 30]
+print(l1)
+del l1[5:7]
+print(l1)
+l1[3::2] = [11,22]
+print(l1)
+#l1[2:5]=100 typeerror: can only assign an iterable
+#如果赋值的对象是一个切片,那么赋值语句的右侧必须是个可以迭代对象.即使只有单独的值,也要把它转换成可迭代的序列
+l1[2:5] = [100]
+print(l1)
+#对序列使用+和*
+l = list(range(3))
+print(l*5)
+print(8*'ab')
+#+号两侧相同类型的数据
+#两个操作符号都遵循着一个规律,不修改原有的操作对象,而是构建一个全新的序列
+#如果在a*n这个语句中,如果序列a里的元素是对其他可变对象的引用的话,需要格外注意
+#如:my_list=[[]]*3来初始化一个有列表组成的列表,你得到的列表包含的3个元素其实是3个引用,这三个引用执行的都是同一列表
+
+#建立有列表组成的列表
+board = [['_']*3 for i in range(3)]
+print(board)
+board[1][2] ='x'
+print(board)
+#wrong example
+weird_board = [['_']*3]*3
+print(weird_board)
+weird_board[1][2] = 'o'
+print(weird_board)
+#same type wrong with last example
+row = ['_']*3
+board = []
+for i in range(3):
+    board.append(row)
+#right example
+board = []
+for i in range(3):
+    row=['_']*3
+    board.append(row)
+print(board)
+board[0][0]='X'
+print(board)
